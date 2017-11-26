f57b3b2dad603fbc9784af4d7b34c1b58fe81f0c
Tomasz Rybicki <tomasz.rybicki7@gmail.com>
2017-11-26 10:56:11 +0100

    initial commit
    Signed-off-by: Tomasz Rybicki <tomasz.rybicki7@gmail.com>

---
+++ src/IndexFile.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : IndexFile.cpp
+// Author      : Tomasz Rybicki
+// Version     :
+// Copyright   : 
+// Description : File organization implemented with indexes utilizing B-trees
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
